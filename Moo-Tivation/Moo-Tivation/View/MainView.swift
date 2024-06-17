71d295c9e5cd5e69e9af70f488e12816f21d2a60
Gimli Kim <infinite1st@gmail.com>
2024-06-17 20:02:41 +0900

    refactor: MainView에 navigation 추가, Stack 띄어쓰기 수정

--- Moo-Tivation/Moo-Tivation/View/MainView.swift
+++ Moo-Tivation/Moo-Tivation/View/MainView.swift
@@ -9,95 +9,97 @@ import SwiftUI
 
 struct MainView: View {
     let segment = ["우유 상태", "완료 여부"]
-       @State private var segmentpick = 0
+    @State private var segmentpick = 0
     
     var body: some View {
-        VStack{
+        NavigationView {
             VStack {
-                Picker(selection: $segmentpick, label: Text("menu")) {
-                    ForEach(0..<2, id: \.self) { index in
-                        Text(self.segment[index]).tag(index)
-                    }
-                }.pickerStyle(SegmentedPickerStyle())
-                    .frame(width: 192)
+                VStack {
+                    Picker(selection: $segmentpick, label: Text("menu")) {
+                        ForEach(0..<2, id: \.self) { index in
+                            Text(self.segment[index]).tag(index)
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .frame(width: 192)
                     
-            }
-            Spacer().frame(height: 22)
-            
-            HStack{
-                Spacer()
+                }
+                Spacer().frame(height: 22)
                 
-                Button(action: {
+                HStack {
+                    Spacer()
+                    NavigationLink(destination: {
+                        SpoilAppSettingView()
+                    }, label: {
+                        Image(systemName: "gearshape.fill")
+                            .resizable()
+                            .frame(width: 25,height: 25)
+                    })
                     
-                }, label: {
-                    Image(systemName: "gearshape.fill")
-                        .resizable()
-                        .frame(width: 25,height: 25)
-                })
-                .padding(.horizontal)
-            }
-            
-            VStack{
-            VStack(alignment: .leading){
-                Text("스포일앱를 사용할 수 있는 시간")
-                    .font(.system(size: 17, weight: .medium))
-                    .foregroundStyle(AppColor.blueTypo)
+                    .padding(.horizontal)
+                }
                 
-                Spacer().frame(height: 5)
-                
-                Text("1시간 20분")
-                    .font(.system(size: 40, weight: .heavy))
-                    .foregroundStyle(AppColor.blackTypo100)
-                
-                Spacer().frame(height: 40)
-                
-                VStack(){
-                    Image("colbycheese")
-                        .resizable()
-                        .frame(width: 224, height: 224)
-                    
-                    Spacer().frame(height: 29)
-                    
-                    Text("사용자가 설정한 문구가 나오는 곳")
-                        .font(.system(size: 20, weight: .bold))
-                        .foregroundStyle(AppColor.blackTypo100)
-                    
-                } .frame(maxWidth: .infinity, alignment: .center)
-                
-                Spacer().frame(height: 49)
-                
-                Text("콜비치즈")
-                    .font(.system(size: 16, weight: .medium))
-                    .foregroundStyle(AppColor.blackTypo70)
-                
-                Spacer().frame(height: 3)
-                
-                Text("프랑스의 노르망디 지방에서 생산되는 부드러운 연질의 \n치즈이다. 카망베르 마을에서 처음 만들어져 그 이름을 땄다.")
-                    .font(.system(size: 13, weight: .regular))
-                    .foregroundStyle(AppColor.blackTypo50)
-                
-                Spacer()
-            }.padding(.horizontal, 44)
-                
-                ZStack{
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(.colorTypo15)
-                        .foregroundColor(.white)
-                        .frame(height: 123)
-                        .padding(.horizontal, 20)
-                    
-                    VStack{
-                        Text("현재 아낀 시간")
-                            .font(.system(size: 17, weight: .regular))
+                VStack {
+                    VStack(alignment: .leading) {
+                        Text("스포일앱를 사용할 수 있는 시간")
+                            .font(.system(size: 17, weight: .medium))
                             .foregroundStyle(AppColor.blueTypo)
                         
-                        Spacer().frame(height: 10)
+                        Spacer().frame(height: 5)
                         
-                        Text("00일 00시간 00분")
-                            .font(.system(size: 32, weight: .bold))
+                        Text("1시간 20분")
+                            .font(.system(size: 40, weight: .heavy))
+                            .foregroundStyle(AppColor.blackTypo100)
+                        
+                        Spacer().frame(height: 40)
+                        
+                        VStack {
+                            Image("colbycheese")
+                                .resizable()
+                                .frame(width: 224, height: 224)
+                            
+                            Spacer().frame(height: 29)
+                            
+                            Text("사용자가 설정한 문구가 나오는 곳")
+                                .font(.system(size: 20, weight: .bold))
+                                .foregroundStyle(AppColor.blackTypo100)
+                            
+                        } .frame(maxWidth: .infinity, alignment: .center)
+                        
+                        Spacer().frame(height: 49)
+                        
+                        Text("콜비치즈")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundStyle(AppColor.blackTypo70)
+                        
+                        Spacer().frame(height: 3)
+                        
+                        Text("프랑스의 노르망디 지방에서 생산되는 부드러운 연질의 \n치즈이다. 카망베르 마을에서 처음 만들어져 그 이름을 땄다.")
+                            .font(.system(size: 13, weight: .regular))
+                            .foregroundStyle(AppColor.blackTypo50)
+                        
+                        Spacer()
+                    }.padding(.horizontal, 44)
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.colorTypo15)
+                            .foregroundColor(.white)
+                            .frame(height: 123)
+                            .padding(.horizontal, 20)
+                        
+                        VStack {
+                            Text("현재 아낀 시간")
+                                .font(.system(size: 17, weight: .regular))
+                                .foregroundStyle(AppColor.blueTypo)
+                            
+                            Spacer().frame(height: 10)
+                            
+                            Text("00일 00시간 00분")
+                                .font(.system(size: 32, weight: .bold))
+                        }
                     }
+                    Spacer().frame(height: 17)
                 }
-                Spacer().frame(height: 17)
             }
         }
     }
