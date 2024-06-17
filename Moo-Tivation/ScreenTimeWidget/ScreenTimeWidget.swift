8fee6bfda9a1acf15463a1529b9b465906e3b8ed
shinseungjae <mok05247@naver.com>
2024-06-17 12:51:21 +0900

    feature: Widget Target 추가

---
+++ Moo-Tivation/ScreenTimeWidget/ScreenTimeWidget.swift
@@ -0,0 +1,80 @@
+//
+//  ScreenTimeWidget.swift
+//  ScreenTimeWidget
+//
+//  Created by 신승재 on 6/17/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct ScreenTimeWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Time:")
+            Text(entry.date, style: .time)
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
+
+struct ScreenTimeWidget: Widget {
+    let kind: String = "ScreenTimeWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                ScreenTimeWidgetEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                ScreenTimeWidgetEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    ScreenTimeWidget()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
---
+++ Moo-Tivation/ScreenTimeWidget/ScreenTimeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ScreenTimeWidgetBundle.swift
+//  ScreenTimeWidget
+//
+//  Created by 신승재 on 6/17/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ScreenTimeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ScreenTimeWidget()
+    }
+}
