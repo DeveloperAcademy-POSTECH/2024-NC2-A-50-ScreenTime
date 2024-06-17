37e4b5177b8bc8eb4c7d3c306d3cc9aa1c4e7dd8
Gimli Kim <infinite1st@gmail.com>
2024-06-17 14:55:19 +0900

    feat: Main Assets Color 추가

---
+++ Moo-Tivation/Moo-Tivation/Assets/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  Moo-Tivation
+//
+//  Created by kyunglimkim on 6/17/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct AppColor {
+    static let blueActive = Color(.colorBlueActive)
+    static let blueTypo = Color(.colorBlueTypo)
+    static let blackTypo100 = Color(.colorTypo100)
+    static let blackTypo70 = Color(.colorTypo70)
+    static let blackTypo50 = Color(.colorTypo50)
+    static let blackTypo25 = Color(.colorTypo25)
+    static let blackTyop15 = Color(.colorTypo15)
+    static let whiteBackround = Color(.settingBackround)
+}
